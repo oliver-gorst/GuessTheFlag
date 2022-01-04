b2d5e55e6d7c585eacbb3ee4b90319f0ee0eda62
Oliver Gorst <oliver.gorst@outlook.com>
2022-01-03 21:21:11 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Oliver Gorst on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
